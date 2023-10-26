1dc350119ee57b6399bec4dba9fe518172e27d32
Deepak Thakur <dt9971678995@gmail.com>
2023-10-26 20:48:57 +0530

    update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -24,7 +24,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_excel("notebook\bookings.xlsx")
+            df = pd.read_excel("notebook/bookings.xlsx")
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
